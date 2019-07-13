9a26aaf6613fb90576a20c9946045fd6fa3f37ef
BjornLuG <bjornlu.dev@gmail.com>
2019-07-13 22:44:53 +0800

    Refactor store box and button

--- store/buttons/script.js
+++ store/buttons/script.js
@@ -1,7 +1,7 @@
 // Slide button
 const slideBtns = document.querySelectorAll('.btn-slide')
-let prevSlideDirection = document.querySelector('.btn-slide-options input[type=radio]:checked').value
-document.querySelectorAll('.btn-slide-options input[type=radio]').forEach(function(radio) {
+let prevSlideDirection = document.querySelector('#btn-slide-options input[type=radio]:checked').value
+document.querySelectorAll('#btn-slide-options input[type=radio]').forEach(function(radio) {
   radio.addEventListener('click', function(e) {
     slideBtns.forEach(function(btn) {
       btn.classList.remove(prevSlideDirection)
@@ -13,13 +13,22 @@ document.querySelectorAll('.btn-slide-options input[type=radio]').forEach(functi
 
 // Line button
 const lineBtns = document.querySelectorAll('.btn-line')
-let prevLinePosition = document.querySelector('.btn-line-options input[type=radio]:checked').value
-document.querySelectorAll('.btn-line-options input[type=radio]').forEach(function(radio) {
+let prevLinePosition = document.querySelector('#btn-line-options input[type=radio]:checked').value
+document.querySelectorAll('#btn-line-options input[type=radio]').forEach(function(radio) {
   radio.addEventListener('click', function() {
     lineBtns.forEach(function(btn) {
       btn.classList.remove(prevLinePosition)
       btn.classList.add(radio.value)
       prevLinePosition = radio.value
     })
+  })
+})
+
+// Pulse button
+const pulseBtns = document.querySelectorAll('.btn-pulse')
+document.querySelector('#btn-pulse-options #pulse-fill').addEventListener('click', function(e) {
+  console.log(e)
+  pulseBtns.forEach(function(btn) {
+    btn.classList.toggle(e.target.value)
   })
 })
