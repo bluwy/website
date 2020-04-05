c3fd38f4a3e773a0966557d2bef781b67849f37a
BjornLuG <bjornlu.dev@gmail.com>
2020-04-05 22:50:04 +0800

    Change names

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bjorn Lu`,
+    description: `Bjorn Lu is a tech enthusiast working with various projects of interest`,
+    author: `Bjorn Lu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Bjorn Lu`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
