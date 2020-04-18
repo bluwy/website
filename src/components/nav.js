79c58704469b46f43324d0a6addb5b8253a2019a
BjornLuG <bjornlu.dev@gmail.com>
2020-04-18 15:29:31 +0800

    Setup nav structure

--- src/components/nav.js
+++ src/components/nav.js
@@ -17,16 +17,27 @@ const Nav = () => {
 
   return (
     <nav className="fixed w-full">
-      <div className="container text-center">
-        <ul className="inline-block">
-          {navLinks.map(link => (
-            <li className="inline-block mx-3 sm:mx-4 my-2" key={link.label}>
-              <Link className="text-lg sm:text-xl" to={link.to}>
-                {link.label}
-              </Link>
-            </li>
-          ))}
-        </ul>
+      <div className="container">
+        <div className="flex flex-row flex-no-wrap justify-between items-center">
+          <Link className="flex flex-row flex-no-wrap items-center" to="/">
+            <img
+              className="mr-1"
+              src="/logo.svg"
+              alt="Bjorn Lu logo"
+              style={{ height: "24px" }}
+            />
+            <span className="font-bold text-lg hidden sm:block">Bjorn Lu</span>
+          </Link>
+          <ul>
+            {navLinks.map(link => (
+              <li className="inline-block mx-3 sm:mx-4 my-2" key={link.label}>
+                <Link className="text-lg" to={link.to}>
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   )
