798321c44020959898414065935852dade7f5a6c
BjornLuG <bjornlu.dev@gmail.com>
2020-05-15 16:07:08 +0800

    Add social data

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,32 +1,44 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Footer = () => (
-  <footer className="mt-12">
-    <div className="container">
-      <hr className="border-t-2 border-primary-700 opacity-50" />
-      <div className="flex flex-row justify-between py-8">
-        <div className="flex-shrink">
-          &copy; Bjorn Lu {new Date().getFullYear()}
-        </div>
-        <div className="flex-shrink">
-          <a
-            className="font-semibold border-b border-black mr-2"
-            href="https://reddit.com/user/IamLUG"
-            title="My Reddit profile"
-          >
-            Reddit
-          </a>
-          <a
-            className="font-semibold border-b border-black"
-            href="https://github.com/BjornLuG"
-            title="My GitHub profile"
-          >
-            GitHub
-          </a>
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allSocialsYaml {
+        nodes {
+          title
+          link
+        }
+      }
+    }
+  `)
+
+  const socialLinks = data.allSocialsYaml.nodes
+
+  return (
+    <footer className="mt-12">
+      <div className="container">
+        <hr className="border-t-2 border-primary-700 opacity-50" />
+        <div className="flex flex-row justify-between py-8">
+          <div className="flex-shrink">
+            &copy; Bjorn Lu {new Date().getFullYear()}
+          </div>
+          <div className="flex-shrink">
+            {socialLinks.map(socialLink => (
+              <a
+                className="font-semibold border-b border-black mr-2"
+                key={socialLink.title}
+                href={socialLink.link}
+                title={`My ${socialLink.title} profile`}
+              >
+                {socialLink.title}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
-  </footer>
-)
+    </footer>
+  )
+}
 
 export default Footer
