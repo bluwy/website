2dfc46ba5fd91525df8e86e410864d08982b94a8
bluwy <bjornlu.dev@gmail.com>
2021-06-07 14:36:20 +0800

    Setup windicss

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
+import { windi } from 'svelte-windicss-preprocess'
 import adapterStatic from '@sveltejs/adapter-static'
 
 export default /** @type {import('@sveltejs/kit').Config} */ ({
+  preprocess: [windi()],
   kit: {
     target: '#svelte',
     adapter: adapterStatic()
