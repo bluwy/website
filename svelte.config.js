823e9051fbaa06f3e945aed7a94e4ef1d51d7020
bluwy <bjornlu.dev@gmail.com>
2021-06-07 14:39:21 +0800

    Add path aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import { windi } from 'svelte-windicss-preprocess'
 import adapterStatic from '@sveltejs/adapter-static'
 
@@ -5,6 +6,16 @@ export default /** @type {import('@sveltejs/kit').Config} */ ({
   preprocess: [windi()],
   kit: {
     target: '#svelte',
-    adapter: adapterStatic()
+    adapter: adapterStatic(),
+    vite: {
+      resolve: {
+        alias: {
+          $assets: path.resolve('src/assets'),
+          $data: path.resolve('src/data'),
+          $posts: path.resolve('src/posts'),
+          $projects: path.resolve('src/projects')
+        }
+      }
+    }
   }
 })
