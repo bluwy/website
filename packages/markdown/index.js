8a19620a540e78ec126d93178833c6c19eecca03
bluwy <bjornlu.dev@gmail.com>
2022-02-13 01:14:13 +0800

    Extract markdown processor as package

---
+++ packages/markdown/index.js
@@ -0,0 +1,54 @@
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkGfm from 'remark-gfm'
+import remarkRehype from 'remark-rehype'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeStringify from 'rehype-stringify'
+import rehypeRaw from 'rehype-raw'
+import { remarkCodeTitle } from './remark-code-title.js'
+import { remarkShiki } from './remark-shiki.js'
+import { remarkData } from './remark-data.js'
+import { rehypeData } from './rehype-data.js'
+
+export function createMarkdownProcessor() {
+  return unified()
+    .use(remarkParse)
+    .use(remarkFrontmatter)
+    .use(remarkGfm)
+    .use(remarkCodeTitle)
+    .use(remarkData)
+    .use(remarkShiki)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings, {
+      behavior: 'append',
+      properties: {
+        ariaHidden: true,
+        tabIndex: -1,
+        className: 'anchor'
+      },
+      content: {
+        type: 'element',
+        tagName: 'svg',
+        properties: {
+          width: '24',
+          height: '24',
+          fill: 'currentColor'
+        },
+        children: [
+          {
+            type: 'element',
+            tagName: 'use',
+            properties: {
+              'xlink:href': '#autolink-icon' // Symbol defined in Icons.svelte
+            }
+          }
+        ]
+      }
+    })
+    .use(rehypeData)
+    .use(rehypeStringify)
+}
--- packages/rollup-plugin-markdown/index.js
+++ packages/rollup-plugin-markdown/index.js
@@ -1,16 +1,4 @@
-import { unified } from 'unified'
-import remarkParse from 'remark-parse'
-import remarkFrontmatter from 'remark-frontmatter'
-import remarkGfm from 'remark-gfm'
-import remarkRehype from 'remark-rehype'
-import rehypeSlug from 'rehype-slug'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypeStringify from 'rehype-stringify'
-import rehypeRaw from 'rehype-raw'
-import { remarkCodeTitle } from './remark-code-title.js'
-import { remarkShiki } from './remark-shiki.js'
-import { remarkData } from './remark-data.js'
-import { rehypeData } from './rehype-data.js'
+import { createMarkdownProcessor } from '@bjornlu/markdown'
 
 /**
  * @returns {{
@@ -19,51 +7,14 @@ import { rehypeData } from './rehype-data.js'
  * }}
  */
 export default function rollupPluginMarkdown() {
-  const remarkPipeline = unified()
-    .use(remarkParse)
-    .use(remarkFrontmatter)
-    .use(remarkGfm)
-    .use(remarkCodeTitle)
-    .use(remarkData)
-    .use(remarkShiki)
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw)
-    .use(rehypeSlug)
-    .use(rehypeAutolinkHeadings, {
-      behavior: 'append',
-      properties: {
-        ariaHidden: true,
-        tabIndex: -1,
-        className: 'anchor'
-      },
-      content: {
-        type: 'element',
-        tagName: 'svg',
-        properties: {
-          width: '24',
-          height: '24',
-          fill: 'currentColor'
-        },
-        children: [
-          {
-            type: 'element',
-            tagName: 'use',
-            properties: {
-              'xlink:href': '#autolink-icon' // Symbol defined in Icons.svelte
-            }
-          }
-        ]
-      }
-    })
-    .use(rehypeData)
-    .use(rehypeStringify)
+  const markdownProcessor = createMarkdownProcessor()
 
   return {
     name: 'rollup-plugin-markdown',
     async transform(code, id) {
       if (!/\.md$/.test(id)) return
 
-      const processed = await remarkPipeline.process(code)
+      const processed = await markdownProcessor.process(code)
 
       const { images, ...processedData } = processed.data
 
