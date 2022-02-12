5b3c9a4d17b5125ce1279ce0497e25631f391b06
bluwy <bjornlu.dev@gmail.com>
2022-02-13 01:30:49 +0800

    Create svelte-preprocess-markdown

---
+++ packages/svelte-preprocess-markdown/index.js
@@ -0,0 +1,18 @@
+import { createMarkdownProcessor } from '@bjornlu/markdown'
+
+export default function sveltePreprocessMarkdown() {
+  const markdownProcessor = createMarkdownProcessor()
+
+  return {
+    /**
+     * @param {{ content: string, filename?: string }} param
+     */
+    async markup({ content, filename }) {
+      if (!filename || !filename.endsWith('.svelte.md')) return
+
+      const result = await markdownProcessor.process(content)
+
+      return result.toString()
+    }
+  }
+}
