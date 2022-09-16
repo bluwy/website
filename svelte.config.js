cbf51c76c031ca0ee29fa6ce139f3ebc828c1d79
bluwy <bjornlu.dev@gmail.com>
2022-09-17 01:23:20 +0800

    Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -3,9 +3,6 @@ import adapter from '@sveltejs/adapter-cloudflare'
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
-    adapter: adapter(),
-    prerender: {
-      default: true
-    }
+    adapter: adapter()
   }
 }
