04a8914b06011d8005ae29bf512c523979bc627f
bluwy <bjornlu.dev@gmail.com>
2023-05-24 11:38:25 +0800

    Fix redirects

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import adapter from '@sveltejs/adapter-cloudflare'
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      routes: {
+        exclude: ['<all>', '/sponsor', '/sponsors', '/todo']
+      }
+    }),
     alias: {
       $assets: path.resolve('src/assets'),
       $data: path.resolve('src/data'),
